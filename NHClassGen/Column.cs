2366ed5008d8d4e97049a6d4d73916bae5132a21
Lucas Teske <lucas@teske.net.br>
2016-07-11 12:15:33 -0300

    Working Class Generator for MSSQL with Enumerator support.

---
+++ NHClassGen/Column.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace NHClassGen {
+  public class Column {
+    #region Constants
+    private static readonly Dictionary<string, Type> dbTypes = new Dictionary<string, System.Type>() {
+      {"bigint", typeof(Int64)},
+      {"binary", typeof(Byte[])},
+      {"bit", typeof(Boolean)},
+      {"char", typeof(String)},
+      {"date", typeof(DateTime)},
+      {"datetime", typeof(DateTime)},
+      {"datetimeoffset", typeof(DateTimeOffset)},
+      {"decimal", typeof(Decimal)},
+      {"float", typeof(Double)},
+      {"image", typeof(Byte[])},
+      {"int", typeof(Int32)},
+      {"money", typeof(Decimal)},
+      {"nchar", typeof(String)},
+      {"ntext", typeof(String)},
+      {"numeric", typeof(Decimal)},
+      {"nvarchar", typeof(String)},
+      {"real", typeof(Single)},
+      {"smalldatetime", typeof(DateTime)},
+      {"smallint", typeof(Int16)},
+      {"text", typeof(String)},
+      {"time", typeof(TimeSpan)},
+      {"timestamp", typeof(Byte[])},
+      {"tinyint", typeof(Byte)},
+      {"varbinary", typeof(Byte[])},
+      {"varchar", typeof(String)}
+    };
+    #endregion
+    #region Private Fields
+    private int _idx;
+    private string _name;
+    private Type _type;
+    private byte _numericPrecision;
+    private short _numericPrecisionRadix;
+    private int _numericScale;
+    private bool _isNullable;
+    private int _characterMaximumLength;
+    private bool _isId;
+    private bool _isAutoIncrement;
+    #endregion
+    #region Properties
+    public int CharacterMaximumLength {
+      get { return _characterMaximumLength; }
+      set { _characterMaximumLength = value; }
+    }
+
+    public int Index {
+      get { return _idx; }
+      set { _idx = value; }
+    }
+
+    public string Name {
+      get { return _name; }
+      set { _name = value; }
+    }
+
+    public Type Type {
+      get { return _type; }
+      set { _type = value; }
+    }
+
+    public byte NumericPrecision {
+      get { return _numericPrecision; }
+      set { _numericPrecision = value; }
+    }
+
+    public short NumericPrecisionRadix {
+      get { return _numericPrecisionRadix; }
+      set { _numericPrecisionRadix = value; }
+    }
+
+    public int NumericScale {
+      get { return _numericScale; }
+      set { _numericScale = value; }
+    }
+
+    public bool IsNullable {
+      get { return _isNullable; }
+      set { _isNullable = value; }
+    }
+
+    public bool IsId {
+      get { return _isId; }
+      set { _isId = value; }
+    }
+
+    public bool IsAutoIncrement {
+      get { return _isAutoIncrement; }
+      set { _isAutoIncrement = value; }
+    }
+    #endregion
+    #region Constructors
+    public Column(DataRow d) {
+      this.Index = d["ORDINAL_POSITION"].GetType() != typeof(DBNull) ? (int)d["ORDINAL_POSITION"] : 0;
+      this.Name = d["COLUMN_NAME"].GetType() != typeof(DBNull) ? (string)d["COLUMN_NAME"] : null; // Just Sanity Checking
+      string t = d["DATA_TYPE"].GetType() != typeof(DBNull) ? (string)d["DATA_TYPE"] : null;
+      if (t != null) {
+        if (dbTypes.ContainsKey(t.ToLower())) {
+          this.Type = dbTypes[t.ToLower()];
+        } else {
+          throw new UnknownTypeException(t);
+        }
+      } else {
+        throw new UnknownTypeException("Null Type");
+      }
+      this.NumericPrecision = d["NUMERIC_PRECISION"].GetType() != typeof(DBNull) ? (byte)d["NUMERIC_PRECISION"] : (byte)0;
+      this.NumericPrecisionRadix = d["NUMERIC_PRECISION_RADIX"].GetType() != typeof(DBNull) ? (short)d["NUMERIC_PRECISION_RADIX"] : (short)0;
+      this.NumericScale = d["NUMERIC_SCALE"].GetType() != typeof(DBNull) ? (int)d["NUMERIC_SCALE"] : 0;
+      this.IsNullable = d["IS_NULLABLE"].GetType() != typeof(DBNull) ? !((string)d["IS_NULLABLE"]).Equals("NO") : false;
+      this.CharacterMaximumLength = d["CHARACTER_MAXIMUM_LENGTH"].GetType() != typeof(DBNull) ? (int)d["CHARACTER_MAXIMUM_LENGTH"] : 0;
+      this.IsAutoIncrement = false;
+      this.IsId = false;
+    }
+    #endregion
+  }
+
+  public class UnknownTypeException : Exception {
+    public UnknownTypeException(string type) : base(string.Format("Unknown Column Type {0}", type)) { }
+  }
+}
---
+++ NHClassGen/GenTools.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NHClassGen {
+  static class GenTools {
+    public static string GenerateClass(string tableName, List<Column> columns, bool generateEnumerator = false) {
+      string className = upperFirst(tableName);
+      string result = "";
+
+      result =
+        "[NHibernate.Mapping.Attributes.Class(Table = \"" + tableName + "\")]\r\n" +
+        "public class " + className + " " + (generateEnumerator ? ": System.Collections.Generic.IEnumerable<string> " : "") + "{\r\n";
+
+      // Private Fields
+      result += "  #region Private Fields\r\n";
+      foreach (Column c in columns) {
+        result += "  private " + c.Type.Name + " _" + lowerFirst(c.Name) + ";\r\n";
+      }
+      result += "  #endregion\r\n";
+
+      // Properties
+      result += "  #region Properties\r\n";
+
+      if (generateEnumerator) {
+        result += "public virtual event EventHandler<" + className + "EventArgs> OnChanged;\r\n";
+      }
+
+      foreach (Column c in columns) {
+        if (c.IsAutoIncrement && c.IsId) {
+          result += "  [NHibernate.Mapping.Attributes.Id(0, Name = \"" + c.Name + "\")]\r\n";
+          result += "  [NHibernate.Mapping.Attributes.Generator(1, Class = \"native\"]\r\n";
+        } else if (c.IsId) {
+          result += "  [NHibernate.Mapping.Attributes.Id(Name = \"" + c.Name + "\")]\r\n";
+        } else if (c.IsAutoIncrement) {
+          result += "  [NHibernate.Mapping.Attributes.Generator(Class = \"native\"]\r\n";
+        } else {
+          result += "  [NHibernate.Mapping.Attributes.Property(";
+          if (!c.IsNullable) {
+            result += "NotNull = true, ";
+          }
+
+          if (c.Type == typeof(decimal)) {
+            result += "Precision = " + c.NumericPrecision + ", ";
+            result += "Scale = " + c.NumericScale + ", ";
+          }
+
+          if (c.Type == typeof(string) && c.CharacterMaximumLength != 0 && c.CharacterMaximumLength != 2147483647) {
+            result += "Length = " + c.CharacterMaximumLength + ", ";
+          }
+
+          if (result[result.Length - 2] == ',') {
+            result = result.Substring(0, result.Length - 2); // Trim last ,
+          }
+
+          result += ")]\r\n";
+        }
+        result += "  public virtual " + c.Type.Name + " " + upperFirst(c.Name) + " {\r\n";
+        result += "    get { return _" + lowerFirst(c.Name) + "; }\r\n";
+        if (generateEnumerator) {
+          result += "    set {\r\n";
+          result += "      " + c.Type.Name + " oldValue = _" + lowerFirst(c.Name) + ";\r\n";
+          result += "      _" + lowerFirst(c.Name) + " = value;\r\n";
+          result += "      OnChangedAction(\"" + upperFirst(c.Name) + "\", oldValue, value);\r\n";
+          result += "    }\r\n";
+        } else {
+          result += "    set { _" + lowerFirst(c.Name) + " = value; }\r\n";
+        }
+        result += "  }\r\n";
+      }
+      result += "  #endregion\r\n";
+
+      if (generateEnumerator) {
+        result += "  #region Methods\r\n";
+        result += "  public virtual System.Collections.Generic.IEnumerator<string> GetEnumerator() {\r\n";
+        foreach (Column c in columns) {
+          if (typeof(string).IsAssignableFrom(c.Type)) {
+            result += "    yield return " + upperFirst(c.Name) + "; \r\n";
+          } else {
+            result += "    yield return " + upperFirst(c.Name) + ".ToString(); \r\n";
+          }
+        }
+
+        result += "  }\r\n";
+        result += "  System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator() {\r\n";
+        result += "    return this.GetEnumerator();\r\n";
+        result += "  }\r\n";
+
+        result += "  protected virtual void OnChangedAction(string FieldName, object oldValue, object newValue) {\r\n";
+        result += "    EventHandler<" + className + "EventArgs> handler = OnChanged;\r\n";
+        result += "    if (handler != null) {\r\n";
+        result += "      " + className + "EventArgs eventArgs = new " + className + "EventArgs();\r\n";
+        result += "      eventArgs.FieldName = FieldName;\r\n";
+        result += "      eventArgs.OldValue = oldValue;\r\n;";
+        result += "      eventArgs.NewValue = newValue;\r\n";
+        result += "      handler(this, eventArgs);\r\n";
+        result += "    }";
+        result += "  }";
+
+        result += "  #endregion\r\n";
+      }
+      // Constructor
+      result += "  #region Constructors\r\n";
+      result += "  public " + className + "() {}\r\n";
+      result += "  public " + className + "(";
+      foreach (Column c in columns) {
+        result += c.Type.Name + " " + lowerFirst(c.Name) + ", ";
+      }
+
+      if (result[result.Length - 2] == ',') {
+        result = result.Substring(0, result.Length - 2); // Trim last ,
+      }
+
+      result += ") {\r\n";
+      foreach (Column c in columns) {
+        result += "    this._" + lowerFirst(c.Name) + " = " + lowerFirst(c.Name) + ";\r\n";
+      }
+      result += "  }\r\n";
+
+      result += "  #endregion\r\n";
+      result += "}";
+
+      if (generateEnumerator) {
+        result += "public class " + className + "EventArgs: EventArgs {\r\n";
+        result += "  public string FieldName { get; set; }\r\n";
+        result += "  public object OldValue { get; set; }\r\n";
+        result += "  public object NewValue { get; set; }\r\n";
+        result += "}";
+      }
+      return result;
+    }
+
+
+    private static string upperFirst(string s) {
+      return s[0].ToString().ToUpper() + s.Substring(1);
+    }
+
+    private static string lowerFirst(string s) {
+      return s[0].ToString().ToLower() + s.Substring(1);
+    }
+  }
+}
